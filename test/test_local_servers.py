84ba913d77b70e93b4e7a969784b9f43fbdce6e6
insar-uk <79936153+insar-uk@users.noreply.github.com>
2023-11-13 10:57:55 +0000

    update test

--- test/test_local_servers.py
+++ test/test_local_servers.py
@@ -10,7 +10,7 @@ assert lock_resource is not None  # Just to shut up the linters who think its un
 @pytest.mark.parametrize("lock_resource", ["port8000"], indirect=True, ids=["Use port 8000"])
 def test_https_server(lock_resource):
     """Launch a http server and get a successful response."""
-    s = ThreadedHttpServer("localhost", 8000)
+    s = ThreadedHttpServer(host="localhost", port=8000)
     s.launch()
     r = requests.get("http://localhost:8000")
     assert r.status_code == 200
@@ -22,7 +22,7 @@ def test_serve_a_directory(lock_resource):
     """Get this directory, try to serve it, see if we can see this file."""
     thisDir, thisFile = os.path.split(__file__)
 
-    s = ThreadedHttpServer("localhost", 8001, directory=thisDir)
+    s = ThreadedHttpServer(host="localhost", port=8001, directory=thisDir)
     s.launch()
     r = requests.get("http://localhost:8001")
     assert r.status_code == 200
