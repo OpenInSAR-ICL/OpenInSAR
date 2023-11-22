dd3c1f6477e33bf0060421bbf1ad19cdf840fcc4
insar-uk <79936153+insar-uk@users.noreply.github.com>
2023-11-22 11:17:29 +0000

    Update octave pytests

--- test/test_octave_connection.py
+++ test/test_octave_connection.py
@@ -9,13 +9,14 @@ import subprocess
 import logging
 import requests
 from src.openinsar_core.HttpJobServer import HttpJobServer
+from typing import Generator, Any
 
 
 JOB_SERVER_PORT = 8080
 OCTAVE_RUN_COMMAND = 'octave-cli'
 
 
-def found_octave():
+def found_octave() -> bool:
     """Check if Octave is installed on the host system."""
     try:
         subprocess.check_output([OCTAVE_RUN_COMMAND, "--version"], shell=True)
@@ -24,12 +25,12 @@ def found_octave():
         return False
 
 
-FOUND_OCTAVE = found_octave()
+FOUND_OCTAVE: bool = found_octave()
 
 
 @pytest.mark.skipif(not found_octave(), reason="Octave not found on command line")
 @pytest.fixture(scope="module", autouse=True)
-def server():
+def server() -> Generator[HttpJobServer, Any, None]:
     """Launch the server, return the server object"""
     from src.openinsar_core.HttpJobServer import HttpJobServer
     server_process = HttpJobServer(port=JOB_SERVER_PORT)
@@ -39,24 +40,21 @@ def server():
 
 
 @pytest.mark.skipif(not found_octave(), reason="Octave not found on command line")
-def test_command_octave():
+def test_command_octave() -> None:
     """Call Octave script from python. Skips if octave-cli is not available on the command line."""
 
     # Get the path to the octave binary
     command = "disp('hello from octave')"
 
-    # Run the octave command
-    o = subprocess.check_output([OCTAVE_RUN_COMMAND, "--eval", command], shell=True)
-
-    # Decode the output
-    o = o.decode('utf-8')
+    # Run the octave command and decode the output
+    o: str = subprocess.check_output([OCTAVE_RUN_COMMAND, "--eval", command], shell=True).decode('utf-8')
 
     # Check if the output is correct
     assert 'hello from octave' in o.lower(), "Octave did not respond as expected"
 
 
 @pytest.mark.skipif(not found_octave(), reason="Octave not found on command line")
-def test_worker_client():
+def test_worker_client() -> None:
     """Call the worker client. See if it is behaving properly."""
     # Get the path to the octave binary
     command = """
@@ -64,19 +62,16 @@ def test_worker_client():
     w = WorkerClient()
     """
     # Remove newlines
-    command = command.replace('\n', ' ')
+    command: str = command.replace('\n', ' ')
 
     # Run the octave command
     try:
-        o = subprocess.check_output([OCTAVE_RUN_COMMAND, "--norc", "--eval", command], stderr=subprocess.STDOUT, shell=True)
+        o: str = subprocess.check_output([OCTAVE_RUN_COMMAND, "--norc", "--eval", command], stderr=subprocess.STDOUT, shell=True).decode('utf-8')
     except subprocess.CalledProcessError as e:
         o = e.output
         logging.warning(e.output)
 
     logging.getLogger().error(o)
-
-    # Decode the output
-    o = o.decode('utf-8')
 
     # Check if the output is correct
     assert not any(x in o.lower() for x in ['error', 'undefined']), "Octave worker client did not respond as expected"
@@ -84,13 +79,13 @@ def test_worker_client():
 
 
 @pytest.mark.skipif(not found_octave(), reason="Octave not found on command line")
-def test_http_communication(server: HttpJobServer):
+def test_http_communication(server: HttpJobServer) -> None:
     """Call the worker client. See if it is behaving properly."""
     # Add a job to the server
     worker_id = 'kevin'
     task = 'test_task'
 
-    response = requests.post(f'http://localhost:{JOB_SERVER_PORT}/add_job', json={'assigned_to': worker_id, 'task': task})
+    response: requests.Response = requests.post(f'http://localhost:{JOB_SERVER_PORT}/add_job', json={'assigned_to': worker_id, 'task': task})
     assert response.status_code == 200, "Request failed"
 
     # Get the path to the octave binary
@@ -103,21 +98,24 @@ def test_http_communication(server: HttpJobServer):
     w.main();
     """
     # Remove newlines and python 4-space indentation
-    command = command.replace('\n', ' ').replace('    ', ' ')
+    command: str = command.replace('\n', ' ').replace('    ', ' ')
 
     # Run the octave command. It should get a job from the server.
     try:
-        o = subprocess.check_output([OCTAVE_RUN_COMMAND, "--norc", "--eval", command], stderr=subprocess.STDOUT, shell=True)
+        o: str = subprocess.check_output([OCTAVE_RUN_COMMAND, "--norc", "--eval", command], stderr=subprocess.STDOUT, shell=True).decode('utf-8')
     except subprocess.CalledProcessError as e:
-        o = e.output
+        o = e.output.decode('utf-8')
         logging.warning(e.output)
-    o = o.decode('utf-8')
 
+    # Is the type of output correct?
+    assert isinstance(o, str), "Output is not a string"
     # Check that the task was found in the output
     assert task.lower() in o.lower(), "Task not found in output"
 
     # Get the job from the server
     response = requests.get(f'http://localhost:{JOB_SERVER_PORT}/get_jobs', json={'assigned_to': worker_id})
     assert response.status_code == 200, "Request failed"
-    match = lambda j: j['assigned_to'] == worker_id and j['task'] == task
-    assert any(match(j) for j in response.json()['jobs']]), "Task not properly assigned to worker"
+
+    def match(j) -> bool:
+        return j['assigned_to'] == worker_id and j['task'] == task
+    assert any(match(j) for j in response.json()['jobs']), "Task not properly assigned to worker"
