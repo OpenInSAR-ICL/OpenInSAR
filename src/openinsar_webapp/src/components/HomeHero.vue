88b9a942b21d58489fc033245e9932b62b2df991
insar-uk <79936153+insar-uk@users.noreply.github.com>
2023-12-06 13:47:25 +0000

    repopulate web app stuff

---
+++ src/openinsar_webapp/src/components/HomeHero.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+
+    <img src="/assets/dungeness-coverage.png" alt="hero" class="hero" />
+</template>
+
+<style scoped>
+.hero {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    /* position: flex; */
+    top: 0;
+    left: 0;
+    min-width: 800px;
+}
+</style>
---
+++ src/openinsar_webapp/src/views/Home.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+    import { RouterView } from 'vue-router';
+    import HomeLinks from '../components/HomeLinks.vue';
+    import HomeHero from '../components/HomeHero.vue';
+</script>
+
+<template>
+    <div>
+        <HomeHero />
+    </div>
+    <div>
+        <HomeLinks />
+    </div>
+</template>
